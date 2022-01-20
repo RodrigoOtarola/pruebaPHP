34ff6f868271577d70ed168ff4061e3c5111d1ff
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-01-20 18:04:47 -0300

    crear ingreso

--- registro.php
+++ registro.php
@@ -1,13 +1,17 @@
 <?php
 
 if(isset($_POST['grabar'])){
-    if (strlen($_POST['rut']) < 1 && strlen($_POST['nombre']) < 1 && 
-    strlen($_POST['a_paterno']) <1 && strlen($_POST['a_materno']) <1){
+    if (strlen($_POST['rut']) >= 1 && strlen($_POST['nombre']) >= 1 && 
+    strlen($_POST['a_paterno']) =>1 && strlen($_POST['a_materno']) >= 1){
         $rut = trim($_POST['rut']);
         $nombre = trim($_POST['nombre']);
         $a_paterno = trim($_POST['a_paterno']);
         $a_materno = trim($_POST['a_materno']);
-        $consulta = "INSERT INTO personas(rut, nombre, a_paterno, a_materno) VALUES ('$rut','$nombre','$a_paterno', '$a_materno')"
+        $consulta = "INSERT INTO personas(rut, nombre, a_paterno, a_materno) VALUES ('$rut','$nombre','$a_paterno', '$a_materno')";
+        $resultado = myslqi_query($link,$consulta);
+        if ($resultado){
+            
+        }
     }
 
 }
