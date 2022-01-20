079233c5d574d9a28c515e00e94553ddb7035a0a
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-01-20 17:31:04 -0300

    ingreso de html

--- index.php
+++ index.php
@@ -11,3 +11,18 @@ $resultado = $gsent->fetchAll();//Devuelve un arreglo.
 var_dump($resultado);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <h1>Hola</h1>
+    <div>
+        
+    </div>
+</body>
+</html>
